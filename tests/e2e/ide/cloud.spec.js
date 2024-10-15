6ac4f2f478a9b22ab89d83f6cccdef1a36c98244
Leandro-Cronapp <leandro.assarice@cronapp.io>
2024-10-15 11:00:03 -0300

    feat: Implementação da Spec e PageObject para testes do Cloud

---
+++ tests/e2e/ide/cloud.spec.js
@@ -0,0 +1,63 @@
+const { InitialNavegate } = require('../../pages/InitialNavegate');
+const { Cloud } = require('../../pages/ide/Cloud');
+const { WebMobile } = require('../../pages/project/lowCode/WebMobile');
+const { test, expect } = require('@playwright/test');
+
+let initialNavegate, webMobile, cloud, newPage, iframe;
+const projectName = 'qa-cron-teste-33';
+
+test.beforeEach(async ({ page, context }) => {
+  test.setTimeout(65000);
+
+  iframe = page.frameLocator('#main');
+  initialNavegate = new InitialNavegate(page, context);
+  webMobile = new WebMobile(page, context);
+  cloud = new Cloud(page, context);
+
+  await initialNavegate.visit();
+  await initialNavegate.login();
+});
+
+test.afterEach(async ({ page, context }, testInfo) => {
+  const image = newPage == null ? await page.screenshot() : await newPage.screenshot();
+  await testInfo.attach('screenshot', {
+    body: image,
+    contentType: 'image/png',
+  });
+  newPage = null;
+});
+
+test('Valida criação de serviço no Cloud', async ({ page, context }, testInfo) => {
+  test.setTimeout(900000);
+
+  await initialNavegate.openProject(projectName);
+  await cloud.accessWindow();
+  await cloud.addService(projectName);
+  await cloud.accessService(projectName);
+
+  newPage = await context.waitForEvent('page');
+  await webMobile.loginRunningWebmobile(newPage);
+  await expect(newPage.getByText('admin@cronapp.io')).toBeVisible();
+});
+
+test('Valida execução de projeto no Cloud', async ({ page, context }) => {
+  test.setTimeout(70000);
+
+  await cloud.accessWindow();
+  await cloud.searchService(projectName, 'initialNavegate');
+  await cloud.accessService(projectName);
+
+  newPage = await context.waitForEvent('page');
+  await webMobile.loginRunningWebmobile(newPage);
+  await expect(newPage.getByText('admin@cronapp.io')).toBeVisible();
+});
+
+test('Valida remoção de serviço do Cloud', async ({ page, context }, testInfo) => {
+  test.setTimeout(70000);
+
+  await cloud.accessWindow();
+  await cloud.searchService(projectName, 'initialNavegate');
+  await cloud.deleteService(projectName);
+
+  await expect(iframe.locator(`//*[text()="${projectName.replace(/-/g, '')}.cloud.cronapp.io"]`)).toBeHidden();
+});
--- tests/pages/ide/Cloud.js
+++ tests/pages/ide/Cloud.js
@@ -1,5 +1,3 @@
-import project from '../config/create-low-code-projects/webMobile.json';
-import ide from '../../config/ide.json';
 import { expect } from '@playwright/test';
 
 export class Cloud {
@@ -8,16 +6,20 @@ export class Cloud {
     this.iframe = page.frameLocator('#main');
   }
 
-  async accessCloudWindow() {
+  async accessWindow() {
     await this.iframe.locator('[ui-id="header-hambuguer"]').click();
     await this.iframe.locator('[ui-id="toolbar-cloudservice"]').click();
+    await this.page.waitForTimeout(3000);
   }
 
-  async searchService(serviceName) {
-    await this.iframe.locator('[tabindex="32"] > *:nth-child(2)').fill(`${serviceName}.cronapp.io`);
-    await this.page.waitForTimeout(3000);
+  async searchService(serviceName, screen) {
+    const inputTabindex = screen == 'initialNavegate' ? '8' : '32';
+    await this.iframe
+      .locator(`[tabindex="${inputTabindex}"] > *:nth-child(2)`)
+      .fill(`${serviceName.replace(/-/g, '')}`);
+    await this.page.waitForTimeout(1500);
     await this.page.keyboard.press('Space');
-    await this.page.waitForTimeout(3000);
+    await this.page.waitForTimeout(2000);
   }
 
   async accessEditOption(serviceName) {
@@ -27,7 +29,64 @@ export class Cloud {
     await expect(this.iframe.locator(`//*[text()="Configuração de ${serviceName}"]`)).toBeVisible();
   }
 
+  async accessService(serviceName) {
+    await this.iframe.locator(`//*[text()="${serviceName.replace(/-/g, '')}.cloud.cronapp.io"]`);
+    await this.iframe.locator(`//*[text()="${serviceName.replace(/-/g, '')}.cloud.cronapp.io"]`).click();
+  }
+
+  async addService(serviceName) {
+    await this.iframe.getByText('Nova Publicação').click();
+    await this.iframe.getByText('Nova Publicação').waitFor({ timeout: 2500 });
+    await this.iframe.getByText('Nova Publicação').nth(2).click();
+
+    // A publicação no Serviços de Cloud gerará cobranças extras, de acordo com seu plano. Deseja continuar?
+    await this.page.waitForTimeout(500);
+    await this.iframe.getByText('Sim').click();
+
+    // Uso de instâncias spot pode levar a indisponibilidades momentâneas não previsíveis. Deseja continuar?
+    await this.page.waitForTimeout(500);
+    await this.iframe.getByText('Sim').click();
+
+    // Deseja gerar o war da aplicação?
+    await this.page.waitForTimeout(500);
+    await this.iframe.getByText('Sim').click();
+
+    // Opções de Geração de Pacote para Deploy
+    await this.page.waitForTimeout(500);
+    await this.iframe.getByText('OK').click();
+
+    // Os dados persistidos nesse tipo de banco poderão ser perdidos, devido à arquitetura distribuída e sem estado do cloud.
+    await this.page.waitForTimeout(1000);
+    await this.iframe.locator('//*[text()="Sim"]').click();
+
+    // Esperar o popup de Progresso sair da tela
+    await this.page.waitForTimeout(10000);
+    await this.iframe.getByText('Progresso').waitFor({ state: 'detached', timeout: 240000 });
+
+    // Procura serviço
+    await this.page.waitForTimeout(20000);
+    await this.searchService(serviceName);
+
+    // Fechar janela de configurações
+    await this.page.waitForTimeout(5000);
+    await this.iframe.locator('//*[text()="Fechar"]').click();
+
+    //Espera o projeto finalizar o carregamento
+    await this.iframe.getByText('Pronto').waitFor({ timeout: 1500000 });
+  }
+
   async serviceUpdate() {
     await this.iframe.locator('//*[text()="Atualizar"]').click();
+  }
+
+  async deleteService(serviceName) {
+    await this.iframe.locator("//div[contains (@style, 'workspace/tree/more.svg')]").click();
+    await this.iframe.locator('//*[text()="Remover"]').click();
+    await this.page.locator(1500);
+    await this.iframe.locator(`//*[text()="Deseja remover '${serviceName.replace(/-/g, '')}'?"]`).waitFor();
+    await this.iframe.locator('//*[text()="Sim"]').click();
+    await this.iframe.locator('[ui-id="dialog-input-text-parent"]').fill('apague');
+    await this.iframe.locator('//*[text()="OK"]').click();
+    await this.page.waitForTimeout(10000);
   }
 }
